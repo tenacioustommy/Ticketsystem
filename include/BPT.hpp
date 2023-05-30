1781f3b49be9ad67dccd0eb16b59593f4d94f562
tenacious <tenacioustommy6@gmail.com>
2023-05-30 22:49:44 +0800

    test

--- include/BPT.hpp
+++ include/BPT.hpp
@@ -1,7 +1,7 @@
 #ifndef BPT_H
 #define BPT_H
 #include"Header.h"
-#define M 80
+#define M 60
 #define CAST(N) reinterpret_cast<char*>(N)
 using std::cin,std::cout,std::endl,std::ios;
 namespace fs=std::filesystem;
